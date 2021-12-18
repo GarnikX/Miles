a8bf7d64c6733ae229633822277a46eff2feab15
Garnik Khachatryan <garniktester@gmail.com>
2021-12-18 18:32:36 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 7545;
+        int miles_bonus = ticket_price / 20;
+        System.out.println("Начислено бонусных миль:" + miles_bonus);
+    }
+}
