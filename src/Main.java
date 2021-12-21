350d698dcabd4341af6db40d37c5003ad5bb6451
Garnik Khachatryan <garniktester@gmail.com>
2021-12-21 19:01:27 +0400

    Names and code refactored

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
+
     public static void main(String[] args) {
-        int ticket_price = 7545;
-        int miles_bonus = ticket_price / 20;
-        System.out.println("Начислено бонусных миль:" + miles_bonus);
+        int ticketPrice = 7545;
+        int milesBonus = ticketPrice / 20;
+        System.out.println("Начислено бонусных миль:" + milesBonus);
     }
 }
